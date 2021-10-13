3febd4fdc82ff6b8f3f30d96c5807f9df711b75c
Skbharath73 <bharadwaj1001@gmail.com>
2021-10-13 16:08:18 +0530

    Commited

---
+++ src/daily/prac/Regex.java
@@ -0,0 +1,13 @@
+package daily.prac;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Regex {
+    public static boolean checkValidation(String reg,String input) {
+        Pattern pattern = Pattern.compile(reg);
+        Matcher matcher = pattern.matcher(input);
+        return matcher.find();
+    }
+
+}
---
+++ src/daily/prac/Student.java
@@ -0,0 +1,49 @@
+package daily.prac;
+
+public class Student {
+    String firstName;
+    String lastName;
+    String phoneno;
+    String email;
+    String password;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneno() {
+        return phoneno;
+    }
+
+    public void setPhoneno(String phoneno) {
+        this.phoneno = phoneno;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
